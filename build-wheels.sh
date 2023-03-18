b9aaaa6955bebd680043f20fdfa6c452fbaa7f24
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-18 15:35:16 +0800

    cicd: install dependency earlier

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,13 +1,4 @@
 cd io
-lsb_release -a
-apt-get update
-apt-get install -y python3-pip
-python -m pip install --upgrade pip
-python -m pip install -e .
-python -m pip install twine
-python -m pip install pexpect
-python -m pip install maturin
-python -m pip install build --upgrade
 python -m build
 cd dist
 ls
