70ed334918feb3fe4c62ee39276b2d5fccaec7fb
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-18 15:43:37 +0800

    debug: image version

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,4 +1,13 @@
 cd io
+cat /etc/*-release
+apt-get update
+apt-get install python-pip3
+python -m pip install --upgrade pip
+python -m pip install -e .
+python -m pip install twine
+python -m pip install pexpect
+python -m pip install maturin
+python -m pip install build --upgrade
 python -m build
 cd dist
 ls
