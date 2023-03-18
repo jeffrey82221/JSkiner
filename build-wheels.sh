2f55675cd1b4a348abaa42c3364d212b9caf7af4
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-18 15:19:34 +0800

    fix: pip command

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,9 +1,10 @@
-python -m pip install --upgrade pip
-pip install -e .
-pip install twine
-pip install pexpect
-pip install maturin
-pip install build --upgrade
+cd io
+python -m pip3 install --upgrade pip
+pip3 install -e .
+pip3 install twine
+pip3 install pexpect
+pip3 install maturin
+pip3 install build --upgrade
 python -m build
 cd dist
 ls
