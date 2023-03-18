51ccf2fe76a8968e9e3c12588b10c3bac12317da
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-18 17:02:25 +0800

    refactor: rename cd yaml and extract python version as parameter

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,3 +1,5 @@
+echo $1
+echo $2
 cd io
 cat /etc/*-release
 python --version
@@ -7,34 +9,34 @@ yum install epel-release -y
 yum makecache
 yum -y install rust
 yum -y install cargo
-# Install Python 3.7
+# Install Python
 yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel xz-devel 
 cd /usr/src
-wget https://www.python.org/ftp/python/3.7.11/Python-3.7.11.tgz
-tar xzf Python-3.7.11.tgz 
-cd Python-3.7.11 
+wget https://www.python.org/ftp/python/$1/Python-$1.tgz
+tar xzf Python-$1.tgz 
+cd Python-$1 
 ./configure --enable-optimizations 
 make altinstall 
-rm /usr/src/Python-3.7.11.tgz 
-python3.7 -V 
+rm /usr/src/Python-$1.tgz 
+$2 -V 
 # Install pip
 yum update -y 
 yum install python3-pip -y
 rpm -qa | grep -i python3-pip
 pip3 -V
 # Install python packages
-python3.7 -m pip3 install --upgrade pip
-python3.7 -m pip3 install -e .
-python3.7 -m pip3 install build --upgrade
+$2 -m pip3 install --upgrade pip
+$2 -m pip3 install -e .
+$2 -m pip3 install build --upgrade
 # Test Package:
 cd /io
 cd examples
 ./test.sh
 cd ..
 # Build wheel
-python3.7 -m build
+$2 -m build
 cd dist
 ls
 # Repair wheel
-python3.7 -m pip3 install auditwheel
+$2 -m pip3 install auditwheel
 auditwheel repair *.whl
