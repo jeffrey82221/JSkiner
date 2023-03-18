b6c2abea0d2d5befea2b3e2df93b410b56ac606d
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-18 16:23:16 +0800

    fix: install py37

--- build-wheels.sh
+++ build-wheels.sh
@@ -7,21 +7,32 @@ yum install epel-release -y
 yum makecache
 yum -y install rust
 yum -y install cargo
+# Install Python 3.7
+yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel xz-devel 
+cd /usr/src
+wget https://www.python.org/ftp/python/3.7.11/Python-3.7.11.tgz
+tar xzf Python-3.7.11.tgz 
+cd Python-3.7.11 
+./configure --enable-optimizations 
+make altinstall 
+rm /usr/src/Python-3.7.11.tgz 
+python3.7 -V 
 # Install pip
 yum update -y 
 yum install python3-pip -y
 rpm -qa | grep -i python3-pip
 pip3 -V
 # Install python packages
-pip3 install --upgrade pip
-pip3 install -e .
-pip3 install build --upgrade
+python3.7 -m pip3 install --upgrade pip
+python3.7 -m pip3 install -e .
+python3.7 -m pip3 install build --upgrade
 # Test Package:
+cd /io
 cd examples
 ./test.sh
 cd ..
 # Build wheel
-python3 -m build
+python3.7 -m build
 cd dist
 ls
 cd ..
