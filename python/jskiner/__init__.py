37edfc79e185648e4b67306f8ff52a288a007ec8
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-18 11:13:00 +0800

    fix: flake8

--- python/jskiner/__init__.py
+++ python/jskiner/__init__.py
@@ -1,12 +1,8 @@
-from .cmd import run
 from . import schema
 from .jskiner import InferenceEngine
 
 __doc__ = jskiner.__doc__
 if hasattr(jskiner, "__all__"):
     __all__ = jskiner.__all__
-
-if __name__ == '__main__':
-    print(run)
-    print(schema)
-    print(InferenceEngine)
+    __all__.append('schema')
+    __all__.append('InferenceEngine')
