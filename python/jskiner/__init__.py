0a42b771db0971ceea14043fdab9691dc883a3c6
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-20 21:27:39 +0800

    refactor: InferenceEngine made return python schema from run

--- python/jskiner/__init__.py
+++ python/jskiner/__init__.py
@@ -1,6 +1,11 @@
 from . import jskiner
 from . import schema  # noqa: F401
-from .jskiner import InferenceEngine  # noqa: F401
+
+class InferenceEngine:
+    def __init__(self, cpu_cnt=1):
+        self._engine = jskiner.InferenceEngine(cpu_cnt)
+    def run(self, batch):
+        return eval(self._engine.run(batch))
 
 __doc__ = jskiner.__doc__
 if hasattr(jskiner, "__all__"):
