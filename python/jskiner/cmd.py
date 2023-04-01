40470fc8399c88791e132cae918fc87fea2673eb
jeffrey82221 <jeffrey82221@gmail.com>
2023-04-01 13:51:27 +0800

    refactor: command line

--- python/jskiner/cmd.py
+++ python/jskiner/cmd.py
@@ -2,7 +2,19 @@ import argparse
 from .jskiner import InferenceEngine
 
 
+
 def run() -> None:
+    args = get_args()
+    print(f"Your json file is at: {args.jsonl}")
+    print('verbose:', args.verbose)
+    schema_str = get_schema_from_jsonl(args.jsonl, worker_cnt=args.nworkers)
+    # Store result to `out` path
+    if args.out != '':
+        with open(args.out, 'w') as f:
+            f.write(schema_str)
+        print('Result saved into', args.out)
+
+def get_args():
     parser = argparse.ArgumentParser(
         description='Inferencing Json Schema')
 
@@ -22,17 +34,20 @@ def run() -> None:
                         type=str, required=False, default='',
                         help="Saving the json schema into a output file")
 
+    parser.add_argument('--split',
+                        type=int, required=False, default=1,
+                        help="Number of splitted jsonl file (1 for no splitting)"
+    )
+    parser.add_argument('--split_path',
+                         type=str, required=False, default='/tmp/split',
+                         help="Path to store the temporary splitted jsonl files"
+    )
     args = parser.parse_args()
-    print(f"Your json file is at: {args.jsonl}")
-    print('verbose:', args.verbose)
+    return args
 
-    with open(args.jsonl, 'r') as f:
+def get_schema_from_jsonl(jsonl_path, worker_cnt=1):
+    with open(jsonl_path, 'r') as f:
         json_list = [x for x in f]
-
-    engine = InferenceEngine(args.nworkers)
+    engine = InferenceEngine(worker_cnt)
     schema_str = engine.run(json_list)
-    if args.verbose:
-        print(schema_str)
-    if args.out != '':
-        with open(args.out, 'w') as f:
-            f.write(schema_str)
+    return schema_str
