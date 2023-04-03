f01e6ca4779fbbaed2338b6152a7744502588f5a
jeffrey82221 <jeffrey82221@gmail.com>
2023-04-03 11:31:33 +0800

    debug: rm cuckoo in the beginning

--- examples/test.sh
+++ examples/test.sh
@@ -1,3 +1,4 @@
+rm cuckoo.pickle
 jskiner --in-path data/small.jsonl --verbose true --out result/small.schema --nworkers 2 --format true --split 2 --split-path ./small_split
 jskiner --in-path data/big.jsonl --verbose true --out result/big.schema --split 5
 jskiner --in-path data/pypi --verbose true --nworkers 2 --out result/pypi.schema --cuckoo-size 1000 --batch-size 2
